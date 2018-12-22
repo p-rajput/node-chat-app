48ab76566a1750af690032ced1c5361a9fd04573
Prabhat singh kaushik <prabhatsingh.1998@gmail.com>
2018-12-23 01:27:49 +0530

    =chat app message is comming

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,28 @@
-var socket= io();
-socket.on('connect',()=>{
- console.log('connected');
-})
-socket.on('disconnect',()=>{
- console.log('we are disconnected!');
-})
-socket.on('newMessage',(message)=>{
-  console.log(message);
-})
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
+});
