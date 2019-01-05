332a8221810f59f71aa40b3c1738b175dd8eb6b0
Prabhat singh kaushik <prabhatsingh.1998@gmail.com>
2019-01-05 17:01:57 +0530

    =chat app is completed

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,39 @@
 var socket = io();
+function scrollToBottom() {
+  var messages=jQuery('#messages');
+  var newMessage=messages.children('li:last-child');
+
+  var clientHeight=messages.prop('clientHeight');
+  var scrollTop=messages.prop('scrollTop');
+  var scrollHeight=messages.prop('scrollHeight');
+  var newMessageHeight=newMessage.innerHeight();
+  var lastMessageHeight=newMessage.prev().innerHeight();
+  if(clientHeight + scrollTop + lastMessageHeight + newMessageHeight >=scrollHeight ){
+    messages.scrollTop(scrollHeight);
+  }
+};
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var param=jQuery.deparam(window.location.search);
+  socket.emit('join',param,function(err){
+    if(err){
+      alert(err);
+      window.location.href='/';
+    }else{
+      console.log('no error');
+    }
+  })
 });
 
 socket.on('disconnect', function () {
-  console.log('Disconnected from server');
 });
-
+socket.on('updateUserList',function(users){
+  var ol=jQuery('<ol></ol>');
+  users.forEach(function(user){
+    ol.append(jQuery('<li></li>').text(user));
+  });
+  jQuery('#users').html(ol);
+});
 socket.on('newMessage', function (message) {
   var formattedTime=moment(message.createdAt).format('h:mm a');
   var template=jQuery('#message-template').html();
@@ -28,7 +54,6 @@ jQuery('#message-form').on('submit', function (e) {
 
 var messageTextBox=jQuery('[name=message]');
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextBox.val()
   }, function () {
         messageTextBox.val(' ');
--- server/server.js
+++ server/server.js
@@ -4,25 +4,52 @@ const socketIo=require('socket.io');
 const http=require('http');
 
 let {generateMessage,generateLocationMessage}=require('./utils/message.js');
+let {isRealString}=require('./utils/validation.js');
+let {Users}=require('./utils/users.js');
 const publicPath=path.join(__dirname,'../public');
 let app=express();
 var server=http.createServer(app);
 var port=process.env.PORT||3000;
 var io=socketIo(server);
+var users=new Users();
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('new user connected');
-  socket.emit('newMessage',generateMessage('Admin','welcome to chat app'));
-  socket.broadcast.emit('newMessage',generateMessage('Admin','New user join'));
+
+  socket.on('join',(params,callback)=>{
+    if(!isRealString(params.name)|| !isRealString(params.room)){
+      return callback('Name and Room is require.');
+    }
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id,params.name,params.room);
+
+    io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+    socket.emit('newMessage',generateMessage('Admin','welcome to chat app'));
+    socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} join`));
+    callback();
+  });
+
   socket.on('createMessage',(message,callback)=>{
-    io.emit('newMessage',generateMessage(message.from,message.text));
+    var user=users.getUser(socket.id);
+    if(user && isRealString(message.text)){
+    io.to(user.room).emit('newMessage',generateMessage(user.name,message.text));
+    }
     callback()
   });
   socket.on('createLocationMessage',(coords)=>{
-    io.emit('generateLocationMessage',generateLocationMessage('admin',coords.latitude,coords.longitude))
+    var user=users.getUser(socket.id);
+    if(user){
+    io.to(user.room).emit('generateLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude))
+  }
   });
   socket.on('disconnect',()=>{
-    console.log('useris dissconnect');
+    var user=users.removeUser(socket.id);
+    if(user){
+      io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left the room`));
+      io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+
+    }
   })
 });
 server.listen(port,()=>{
